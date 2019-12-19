b14f19679a8365d9d5a181739c1fba1f0cd51c0d
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2019-12-19 15:35:31 -0500

    Update abuse.js

--- abuse.js
+++ abuse.js
@@ -1,6 +1,6 @@
 function open_window() {
-   location.href("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/formResponse");
-   location.href("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/viewform?usp=form_confirm");
+   location.href = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/formResponse";
+   location.href = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/viewform?usp=form_confirm";
    
 }
 
