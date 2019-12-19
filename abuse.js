42149610e87c0870525ff0db43e94a3d6279de01
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2019-12-19 15:34:27 -0500

    Update abuse.js

--- abuse.js
+++ abuse.js
@@ -1,6 +1,6 @@
 function open_window() {
-   window.open("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/formResponse");
-   window.open("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/viewform?usp=form_confirm");
+   location.href("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/formResponse");
+   location.href("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/viewform?usp=form_confirm");
    
 }
 
