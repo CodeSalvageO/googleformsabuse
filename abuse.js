8162175f7a74f15c5d7d627a084a456130cd393f
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2019-12-19 15:29:13 -0500

    Create abuse.js

---
+++ abuse.js
@@ -0,0 +1,5 @@
+function open_window() {
+   window.open("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/formResponse");
+   window.open("https://docs.google.com/forms/u/1/d/e/1FAIpQLSdxvKeMhNVpGGJwalmYKvBStpgmPM93sSo6DbCJde7cTwI7Ow/viewform?usp=form_confirm");
+   
+}
